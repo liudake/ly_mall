3c219150cc5537d6bc585330e5f0c8d6b92ecdf8
liuqi <liuqi@sunfo168.com>
2018-11-23 15:45:22 +0800

    add 上拉加载和下拉刷新组件和功能

---
+++ miniprogram/src/service/Home/homeConfig.js
@@ -0,0 +1,7 @@
+export const getTabConfig = {
+  name: 'getTabMall',
+  method: 'post',
+  desc: 'tab商品列表',
+  path: `/product/declaration/appointmentList`,
+  mockPath: `/product/declaration/appointmentList`
+};
